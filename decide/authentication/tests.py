6a6df24bc18dc8f25e13bcec55d2edb3a14f0343
siinergia <sinergiaftw@gmail.com>
2020-01-30 18:13:37 +0100

    solucion para los tests

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -110,7 +110,7 @@ class AuthTestCase(APITestCase):
         data = {'username': 'potato', 'password': '123'}
         response = self.client.post(
             '/authentication/login/', data, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(Token.objects.filter(
             user__username='voter1').count(), 1)
 
@@ -129,7 +129,7 @@ class AuthTestCase(APITestCase):
         data = {'username': 'patata', 'password': '123'}
         response = self.client.post(
             '/authentication/login/', data, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(Token.objects.filter(
             user__username='voter1').count(), 1)
 
@@ -148,7 +148,7 @@ class AuthTestCase(APITestCase):
         data = {'username': 'petate', 'password': '123'}
         response = self.client.post(
             '/authentication/login/', data, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 400)
         self.assertEqual(Token.objects.filter(
             user__username='voter1').count(), 1)
 
